404c26311bef27e21abc0c0525d954ab3d61a177
expresso-patronum <sabrina.rf2003@gmail.com>
2021-09-22 21:17:31 -0300

    sabrina

--- cpf.php
+++ cpf.php
@@ -1,12 +1,29 @@
 <html>
 <body>
 <?php
-$cpf = $_POST["cpf"];
 function validaCPF($cpf) {
-if (strlen($cpf) <= 11) {
-   ECHO 'ERRO';
-} 
-} 
+    $cpf = $_POST['cpf'];
+    if (strlen($cpf) != 11) { return false; }
+    for ($i = 9; $i < 11; $i++) {
+        for ($d = 0, $c = 0; $c < $i; $c++) {
+            $d += $cpf[$c] * (($i + 1) - $c);
+        }
+        $d = ((10 * $d) % 11) % 10;
+        if ($cpf[$c] != $d) {
+            return false;
+        }
+    }
+    return true;
+}
+if (isset($_POST['botao1'])) {
+    $cpf = $_POST['cpf'];
+    if (validaCPF($cpf) == true) {
+        echo $_POST["cpf"];
+    } else {
+        echo 'CPF inválido';
+    }
+}
+
 ?>
 </body>
 </html>
