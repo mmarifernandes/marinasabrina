c1bbaf0a046e15a887e88060d62d2d66889fab38
mmarifernandes <marinafernandess103@gmail.com>
2021-09-27 21:14:02 -0300

    pronto mas c problema no pattern vo me m

--- calcextenso.php
+++ calcextenso.php
@@ -5,6 +5,8 @@
 var_dump($_POST);
 $numeroextenso = $_POST['numeroextenso'];
 $numeroextenso2 = $_POST['numeroextenso2'];
+$operacao = $_POST['operador'];
+echo $operacao;
 
 function converter($numeroextenso){
 
@@ -49,56 +51,50 @@ $numbers = array(
     'milhao' => 1000000,
     'milhoes' => 1000000,
     'bilhoes' => 1000000000,
-    'bilhao' => 1000000000);
+    'bilhao' => 1000000000,
+    'milhão' => 1000000,
+    'milhões' => 1000000,
+    'bilhões' => 1000000000,
+    'bilhão' => 1000000000);
 
-//first we remove all unwanted characters... and keep the text
-$str = str_replace(", ", " ", $numeroextenso);
-echo $str;
-//now we explode them word by word... and loop through them
+$str = str_replace(",", " e", $numeroextenso);
+
+$numeroextenso = str_replace(",", " e", $numeroextenso);
+$numeroextenso = str_replace("ã", "a", $numeroextenso);
+$numeroextenso = str_replace("õ", "o", $numeroextenso);
 $words = explode(" ", $numeroextenso);
-echo $words;
-//i devide each thousands in groups then add them at the end
-//For example 2,640,234 "two million six hundred and fourty thousand two hundred and thirty four"
-//is defined into 2,000,000 + 640,000 + 234
-
-//the $total will be the variable were we will add up to
+// echo $numeroextenso;
 $total = 1;
 
-//flag to force the next operation to be an addition
 $force_addition = false;
 
-//hold the last digit we added/multiplied
 $last_digit = null;
 
-//the final_sum will be the array that will hold every portion "2000000,640000,234" which we will sum at the end to get the result
 $final_sum = array();
 
 foreach ($words as $word) {
 
-    //if its not an and or a valid digit we skip this turn
     if (!isset($numbers[$word]) && $word != "e") {
         continue;
     }
 
-    //all small letter to ease the comparaison
     $word = strtolower($word);
 
-    //if it's an and .. and this is the first digit in the group we set the total = 0 
-    //and force the next operation to be an addition
     if ($word == "e") {
         if ($last_digit === null) {
             $total = 0;
+
         }
         $force_addition = true;
     } else {
-        //if its a digit and the force addition flag is on we sum
+
         if ($force_addition) {
             $total += $numbers[$word];
             $force_addition = false;
+
         } else {
-            //if the last digit is bigger than the current digit we sum else we multiply
-            //example twenty one => 20+1,  twenty hundred 20 * 100
-            if ($last_digit !== null && $last_digit > $numbers[$word]) {
+
+            if ($last_digit !== null && $last_digit >= $numbers[$word]) {
                 $total += $numbers[$word];
             } else {
                 $total *= $numbers[$word];
@@ -106,27 +102,53 @@ foreach ($words as $word) {
         }
         $last_digit = $numbers[$word];
 
-        //finally we distinguish a group by the word thousand, million, billion  >= 1000 ! 
-        //we add the current total to the $final_sum array clear it and clear all other flags...
         if ($numbers[$word] >= 1000) {
-            $final_sum[] = $total;
+            $final_sum[] = $total ;
             $last_digit = null;
             $force_addition = false;
             $total = 0;
         }
     }
 }
-
-// there is your final answer !
 $final_sum[] = $total;
-
 return array_sum($final_sum);
 
 };
 
-echo converter($numeroextenso);
+$valor1 = converter($numeroextenso);
+$valor2 = converter($numeroextenso2);
+if($operacao == '+'){
+    $total = $valor1 + $valor2; 
+
+}
+if($operacao == '*'){
+    $total = $valor1 * $valor2; 
+
+}
+if($operacao == '-'){
+    $total = $valor1 - $valor2; 
+
+}
+if($operacao == '/'){
+    $total = $valor1 / $valor2; 
+
+}
+
+    echo "<center>";
+    echo "<br>";
+    echo "<h1>";
+    echo $total;
+    echo "</h1>";
+    echo "<br>";
+echo "<center>";
 echo "<br>";
+echo "<h1>";
+echo converter($numeroextenso);
+echo "</h1>";
+echo "<br>";
+echo "<h1>";
 echo converter($numeroextenso2);
+echo "</h1>";
 
 ?>
 </body>
