48bed07cf07e12af6ba9d22a1f6591fef33846ca
mmarifernandes <marinafernandess103@gmail.com>
2021-09-30 20:31:32 -0300

    ainda falta validar

--- calcextenso.php
+++ calcextenso.php
@@ -10,108 +10,82 @@ echo $operacao;
 
 function converter($numeroextenso){
 
-$numbers = array(
-    'um' => 1,
-    'dois' => 2,
-    'tres' => 3,
-    'quatro' => 4,
-    'cinco' => 5,
-    'seis' => 6,
-    'sete' => 7,
-    'oito' => 8,
-    'nove' => 9,
-    'dez' => 10,
-    'onze' => 11,
-    'doze' => 12,
-    'treze' => 13,
-    'quatorze' => 14,
-    'quinze' => 15,
-    'dezesseis' => 16,
-    'dezessete' => 17,
-    'dezoito' => 18,
-    'dezenove' => 19,
-    'vinte' => 20,
-    'trinta' => 30,
-    'quarenta' => 40,
-    'cinquenta' => 50,
-    'sessenta' => 60,
-    'setenta' => 70,
-    'oitenta' => 80,
-    'noventa' => 90,
-    'cem' => 100,
-    'cento' => 100,
-    'duzentos' => 200,
-    'trezentos' => 300,
-    'quatrocentos' => 400,
-    'quinhentos' => 500,
-    'seiscentos' => 600,
-    'setecentos' => 700,
-    'novecentos' => 900,
-    'mil' => 1000,
-    'milhao' => 1000000,
-    'milhoes' => 1000000,
-    'bilhoes' => 1000000000,
-    'bilhao' => 1000000000,
-    'milhão' => 1000000,
-    'milhões' => 1000000,
-    'bilhões' => 1000000000,
-    'bilhão' => 1000000000);
+$numeros = array('um' => 1, 'dois' => 2, 'tres' => 3, 'quatro' => 4, 'cinco' => 5, 'seis' => 6, 'sete' => 7, 'oito' => 8, 'nove' => 9, 'dez' => 10, 'onze' => 11, 'doze' => 12,
+    'treze' => 13, 'quatorze' => 14, 'quinze' => 15, 'dezesseis' => 16, 'dezessete' => 17, 'dezoito' => 18, 'dezenove' => 19, 'vinte' => 20, 'trinta' => 30,
+    'quarenta' => 40, 'cinquenta' => 50, 'sessenta' => 60, 'setenta' => 70, 'oitenta' => 80, 'noventa' => 90, 'cem' => 100, 'cento' => 100, 'duzentos' => 200,
+    'trezentos' => 300, 'quatrocentos' => 400, 'quinhentos' => 500, 'seiscentos' => 600, 'setecentos' => 700, 'novecentos' => 900, 'mil' => 1000,
+    'milhao' => 1000000, 'milhoes' => 1000000);
 
 $str = str_replace(",", " e", $numeroextenso);
 
 $numeroextenso = str_replace(",", " e", $numeroextenso);
 $numeroextenso = str_replace("ã", "a", $numeroextenso);
+$numeroextenso = str_replace("á", "a", $numeroextenso);
+$numeroextenso = str_replace("à", "a", $numeroextenso);
+$numeroextenso = str_replace("â", "a", $numeroextenso);
+$numeroextenso = str_replace("é", "e", $numeroextenso);
+$numeroextenso = str_replace("è", "e", $numeroextenso);
+$numeroextenso = str_replace("ê", "e", $numeroextenso);
 $numeroextenso = str_replace("õ", "o", $numeroextenso);
+$numeroextenso = str_replace("ó", "o", $numeroextenso);
+$numeroextenso = str_replace("ò", "o", $numeroextenso);
+$numeroextenso = str_replace("ô", "o", $numeroextenso);
+$numeroextenso = str_replace("í", "i", $numeroextenso);
+$numeroextenso = str_replace("ì", "i", $numeroextenso);
+$numeroextenso = str_replace("ú", "u", $numeroextenso);
+$numeroextenso = str_replace("ù", "u", $numeroextenso);
+$numeroextenso = str_replace("û", "u", $numeroextenso);
+
 $words = explode(" ", $numeroextenso);
-// echo $numeroextenso;
+$ultimo = '';
+$resultado = 0;
+$somar = 0;
 $total = 1;
 
-$force_addition = false;
-
-$last_digit = null;
-
-$final_sum = array();
-
 foreach ($words as $word) {
-
-    if (!isset($numbers[$word]) && $word != "e") {
-        continue;
-    }
-
     $word = strtolower($word);
 
-    if ($word == "e") {
-        if ($last_digit === null) {
-            $total = 0;
-
+    if($word == "e") {
+        $somar = 1;
+        continue;
+    }
+    else{
+        if ($somar == 1) {
+            $total += $numeros[$word];
+            // echo("<script>console.log('PHP: " . 'somar = 1'. "');</script>");
+            // echo("<script>console.log('PHP: " . 'somou o e'. "');</script>");
+            // echo("<script>console.log('PHP: " . $total. "');</script>");
+            $somar = 0;
         }
-        $force_addition = true;
-    } else {
-
-        if ($force_addition) {
-            $total += $numbers[$word];
-            $force_addition = false;
-
-        } else {
-
-            if ($last_digit !== null && $last_digit >= $numbers[$word]) {
-                $total += $numbers[$word];
-            } else {
-                $total *= $numbers[$word];
+        else if($somar == 0){
+            // echo("<script>console.log('PHP: " . 'somar = 0'. "');</script>");
+            if($ultimo !== '' && $ultimo > $numeros[$word]) {
+                //somar
+                // echo("<script>console.log('PHP: " . $total. "');</script>");
+                $total += $numeros[$word];
+                // echo("<script>console.log('PHP: " . 'somou'. "');</script>");
+            }else{
+                //multiplicar
+                // echo("<script>console.log('PHP: " . $total. "');</script>");
+                $total *= $numeros[$word];
+                // echo("<script>console.log('PHP: " . 'multiplicou'. "');</script>");
+                // echo("<script>console.log('PHP: " . $total. "');</script>");
             }
         }
-        $last_digit = $numbers[$word];
-
-        if ($numbers[$word] >= 1000) {
-            $final_sum[] = $total ;
-            $last_digit = null;
-            $force_addition = false;
+        $ultimo = $numeros[$word];
+        // echo("<script>console.log('PHP: " . 'ultimo = numero anterior'. "');</script>");
+        if($numeros[$word] >= 1000) {
+            $resultado += $total;
+            // echo("<script>console.log('PHP: " . '>1000 resultado recebe total'. "');</script>");
+            // echo("<script>console.log('PHP: " . $resultado. "');</script>");
+            $ultimo = '';
+            $somar = 0;
             $total = 0;
         }
     }
 }
-$final_sum[] = $total;
-return array_sum($final_sum);
+$resultado += $total;
+return $resultado;
 
 };
 
@@ -134,22 +108,21 @@ if($operacao == '/'){
 
 }
 
-    echo "<center>";
-    echo "<br>";
-    echo "<h1>";
-    echo $total;
-    echo "</h1>";
-    echo "<br>";
+    
 echo "<center>";
 echo "<br>";
 echo "<h1>";
 echo converter($numeroextenso);
-echo "</h1>";
-echo "<br>";
-echo "<h1>";
+echo $operacao;
 echo converter($numeroextenso2);
 echo "</h1>";
-
+echo "<h1>";
+echo "=";
+echo "</h1>";
+echo "<h1>";
+echo $total;
+echo "</h1>";
+echo "<br>";
 ?>
 </body>
 </html>
---
+++ valor2.php
@@ -0,0 +1,19 @@
+<html>
+<body>
+<?php
+var_dump($_POST);
+$valor = $_POST['valor'];
+
+// Transcrever um valor monetário
+// front-end HTML/JS
+// input text valor, deve ser um valor monetário válido entre 0,01 e 999999999,99
+// button enviar, só deve enviar se o valor monetário for válido
+
+// back-end PHP
+// mostra uma mensagem de erro se o valor monetário for inválido
+// só deve transcrever e mostrar o valor por extenso se o valor monetário for válido
+
+
+?>
+</body>
+</html>
