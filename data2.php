c9f8695d318f4897b53523728be3b529b58b2262
mmarifernandes <marinafernandess103@gmail.com>
2021-10-08 15:55:52 -0300

    aaaa

--- data2.php
+++ data2.php
@@ -250,3 +250,105 @@ Training
 Blog
 About
 Loading complete
+
+
+
+<html>
+<body>
+<?php
+// var_dump($_POST);
+// echo "<br>";
+// echo "<br>";
+# '/^M{0,3}(CM|CD|D?C{0,3})(XC|XL|L?X{0,3})(IX|IV|V?I{0,3})$/' pattern
+
+$numeros = array($_POST);
+$operadores = array(
+    $_POST['operador1'], $_POST['operador2'], $_POST['operador3'], $_POST['operador4'], $_POST['operador5'], $_POST['operador6'], $_POST['operador7'], $_POST['operador8'], $_POST['operador9'], $_POST['operador10']
+);
+
+function converte($romano1) {
+    $romanos = array(
+        'M' => 1000,
+        'CM' => 900,
+        'D' => 500,
+        'CD' => 400,
+        'C' => 100,
+        'XC' => 90,
+        'L' => 50,
+        'XL' => 40,
+        'X' => 10,
+        'IX' => 9,
+        'V' => 5,
+        'IV' => 4,
+        'I' => 1
+    );
+    $result = 0;
+
+
+foreach ($romanos as $key => $valor) {
+    while (strpos($romano1, $key) === 0) {
+        $result += $valor;
+        $romano1= substr($romano1, strlen($key));
+    }
+}
+	return $result;
+}
+// echo converte($romano1);
+// $valor1 = converte($valor1);
+// echo converte($valor[$i]);
+for ($i = 0; $i<11; $i++){
+    $total = converte($_POST[2]);
+
+    
+    // if (($index = array_search('+', $operadores)) !== false) {
+    //     $total += converte($_POST[$i]);
+    //     // array_splice($numeros, $index - 1, 3, $numeros[$index - 1] + $numeros[$index + 1]);
+    // }
+        if (($index = array_search('+', $operadores)) !== false) {
+        $total += converte($_POST[$i]);
+     
+        // array_splice($numeros, $index - 1, 3, $numeros[$index - 1] + $numeros[$index + 1]);
+    }
+    //     if (($index = array_search('*', $operadores)) !== false) {
+    //         echo 'aaa';
+    //     $total += $total * converte($numeros[0][$i]);
+    //     // array_splice($numeros, $index - 1, 3, $numeros[$index - 1] + $numeros[$index + 1]);
+    // }
+}
+echo $total;
+// $numeros = array();
+    // $numeros[] = converte($_POST[$i]); 
+    // echo  converte($_POST[$i]); // mostra todos os valores
+    // echo '<br>';
+    // if($_POST['operador'.$i] == '+'){
+        // $total += converte($_POST[$i]);
+        // echo 'aaaa';
+        // }
+        // if($_POST['operador'.$i] == '-'){
+            // $total = $total - converte($_POST[$i]);
+            // echo $total;
+            // }
+            
+
+    // while ($_POST['operador'.$i] == '+'){
+    //      $total += converte($_POST[$i]);
+    // }
+    //     while ($_POST['operador'.$i] == '-'){
+    //     $total = $total - converte($_POST[$i]);
+    // }
+    //         if($_POST['operador'.$i] == '*'){
+    // echo $total = converte($_POST[$i]) * converte($_POST[$i-1]);
+    // }
+    //         if($_POST['operador'.$i] == '/'){
+    // echo $total = converte($_POST[$i]) / converte($_POST[$i-1]);
+    // }
+    // echo '<br>';
+    // echo $_POST['operador'.$i];
+
+print_r ($numeros);
+print_r ($operadores);
+// echo $total;
+
+?>
+</body>
+</html>
--- romanos.php
+++ romanos.php
@@ -1,13 +1,15 @@
 <html>
 <body>
 <?php
-var_dump($_POST);
-echo "<br>";
-echo "<br>";
+// var_dump($_POST);
+// echo "<br>";
+// echo "<br>";
 # '/^M{0,3}(CM|CD|D?C{0,3})(XC|XL|L?X{0,3})(IX|IV|V?I{0,3})$/' pattern
-$romano1 = $_POST["1"];
-$romano2 = $_POST['2'];
 
+$numeros = array($_POST);
+$operadores = array(
+    $_POST['operador1'], $_POST['operador2'], $_POST['operador3'], $_POST['operador4'], $_POST['operador5'], $_POST['operador6'], $_POST['operador7'], $_POST['operador8'], $_POST['operador9'], $_POST['operador10']
+);
 
 function converte($romano1) {
     $romanos = array(
@@ -39,26 +41,58 @@ foreach ($romanos as $key => $valor) {
 // echo converte($romano1);
 // $valor1 = converte($valor1);
 // echo converte($valor[$i]);
-for ($i = 10; $i>0; $i--){
+for ($i = 0; $i<11; $i++){
+    $total = converte($_POST[2]);
+
     
-    echo  converte($_POST[$i]); // mostra todos os valores
-    echo '<br>';
-    if($_POST['operador'.$i] == '+'){
-    echo $total = converte($_POST[$i]) + converte($_POST[$i-1]);
+    // if (($index = array_search('+', $operadores)) !== false) {
+    //     $total += converte($_POST[$i]);
+    //     // array_splice($numeros, $index - 1, 3, $numeros[$index - 1] + $numeros[$index + 1]);
+    // }
+        if (($index = array_search('+', $operadores)) !== false) {
+        $total += converte($_POST[$i]);
+     
+        // array_splice($numeros, $index - 1, 3, $numeros[$index - 1] + $numeros[$index + 1]);
     }
-        if($_POST['operador'.$i] == '-'){
-    echo $total = converte($_POST[$i]) - converte($_POST[$i-1]);
-    }
-            if($_POST['operador'.$i] == '*'){
-    echo $total = converte($_POST[$i]) * converte($_POST[$i-1]);
-    }
-            if($_POST['operador'.$i] == '/'){
-    echo $total = converte($_POST[$i]) / converte($_POST[$i-1]);
-    }
-    echo '<br>';
-    echo $_POST['operador'.$i];
+    //     if (($index = array_search('*', $operadores)) !== false) {
+    //         echo 'aaa';
+    //     $total += $total * converte($numeros[0][$i]);
+    //     // array_splice($numeros, $index - 1, 3, $numeros[$index - 1] + $numeros[$index + 1]);
+    // }
 }
 echo $total;
+// $numeros = array();
+    // $numeros[] = converte($_POST[$i]); 
+    // echo  converte($_POST[$i]); // mostra todos os valores
+    // echo '<br>';
+    // if($_POST['operador'.$i] == '+'){
+        // $total += converte($_POST[$i]);
+        // echo 'aaaa';
+        // }
+        // if($_POST['operador'.$i] == '-'){
+            // $total = $total - converte($_POST[$i]);
+            // echo $total;
+            // }
+            
+
+    // while ($_POST['operador'.$i] == '+'){
+    //      $total += converte($_POST[$i]);
+    // }
+    //     while ($_POST['operador'.$i] == '-'){
+    //     $total = $total - converte($_POST[$i]);
+    // }
+    //         if($_POST['operador'.$i] == '*'){
+    // echo $total = converte($_POST[$i]) * converte($_POST[$i-1]);
+    // }
+    //         if($_POST['operador'.$i] == '/'){
+    // echo $total = converte($_POST[$i]) / converte($_POST[$i-1]);
+    // }
+    // echo '<br>';
+    // echo $_POST['operador'.$i];
+
+print_r ($numeros);
+print_r ($operadores);
+// echo $total;
 
 ?>
 </body>
