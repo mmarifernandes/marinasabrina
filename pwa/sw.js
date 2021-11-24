0370c60ebae90f59fbabf40bf9d638d97342a11a
Marina Fernandes <47857432+mmarifernandes@users.noreply.github.com>
2021-11-23 21:13:05 -0300

    Add files via upload

---
+++ pwa/sw.js
@@ -0,0 +1,30 @@
+let CACHE_NAME = 'my-site-cache-v1';
+let urlsToCache = [
+  './index.html',
+  './image/raio.png',
+  './image/pwa.png',
+  './',
+  './manifest.json'
+];
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
